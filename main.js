97759d2c80e742bbb7c9c93fc7472fe6c7c31b50
Пользователь <odinaabdurahimova03@gmail.com>
2024-01-09 08:11:42 +0500

    Added files

---
+++ main.js
@@ -0,0 +1,22 @@
+alert("HUSH KELIBSIZ")
+let ism=prompt("To`liq ismingiz nima?", "Aliyev Ali")
+let age=prompt("Tug`ulgan sanangiz?", "01 01 1990")
+let region=prompt("Yashash joyingiz?", "Namangan")
+let number=prompt("Telefon raqamingiz?", "998999999")
+let family=prompt("oilalimisiz?", "Yo`q")
+let stadi=prompt("Ta`lim olayotgan oliygohingiz?", "Yo`q")
+let language=prompt("Nechta tilni bilasiz?", "1 ta")
+let work=prompt("Ish tajribangiz qancha?", "1 yil")
+let programming=prompt("Qaysi dasturlash tillarini bilasiz?", "Web dasturlash. Mobile dastulash, ...")
+let chess=prompt("Shaxmat o`ynashni yoqtirasizmi?", "Ha")
+alert("TASHRIFINGIZDAN MINNATDORMIZ")
+console.log(ism);
+console.log(age);
+console.log(region);
+console.log(number);
+console.log(family);
+console.log(stadi);
+console.log(language);
+console.log(work);
+console.log(programming);
+console.log(chess);
